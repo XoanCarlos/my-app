7b9701025b20bb0d7050d0e6ffa9990b21301bb1
XoanCarlos <jcarlosperezgonzalez@gmail.com>
2023-04-17 21:27:55 +0200

    firs commit

---
+++ src/models/task.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const task = new Schema({
+    destino: String,
+    distancia: Number,
+    precio: Number
+    });
+
+module.exports = mongoose.model('task', task);
---
+++ src/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+//express configuration
+app.set('port', process.env.PORT || 3000);
+
+
+//middlwera
+app.use(morgan('combined'));
+app.use(express.json());
+
+
+//routes
+app.use('/travel', require('./routes/travel'));
+
+
+//static files
+app.use(express.static(__dirname + '/public'))
+
+
+//server listening
+mongoose.connect('mongodb://127.0.0.1/myapp')
+    .then(db => console.log('Database connected'))
+    .catch(err => console.log(err));
+
+app.listen(app.get('port'), () => {
+    console.log('Server listenning in port', app.get('port'));
+});
