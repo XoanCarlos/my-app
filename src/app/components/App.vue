5b99faba5a1eb75479fffae96a76bee110ff1e13
XoanCarlos <jcarlosperezgonzalez@gmail.com>
2023-05-02 21:27:45 +0200

    menu

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1> Albanian Project </h1>
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="collpase navbar-collapse">
+            <a class="navbar-brand" href="/">
+            <img src="img/taxi.svg" alt="" width="50" height="40" class="d-inline-block align-text-top">    
+            </a>    
+            <a href="/" class="navbar-brand nav-link active">Home</a>
+            <a href="#" class="navbar-brand">Services</a>
+            <a href="#" class="navbar-brand">Fleet</a>
+            <a href="#" class="navbar-brand">About</a>
+            <div class="container">
+                <form class="d-flex">
+                  <input class="col-xs-3" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+          </div>
+        </nav>
+     </div> 
 </template>
