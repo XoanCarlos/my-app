516e6767c0e3f52b03329b65cc35d1d965fc5116
XoanCarlos <jcarlosperezgonzalez@gmail.com>
2023-05-04 21:44:30 +0200

    new file:   src/app/components/AboutUs.vue 	modified:   src/app/components/App.vue 	new file:   src/app/components/HomeUs.vue 	deleted:    src/app/components/Services.vue 	new file:   src/app/components/ServicesItem.vue 	new file:   src/app/components/TaxiFleet.vue 	modified:   src/public/js/bundle.js 	modified:   src/router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Services from '../app/components/Services.vue'
+import ServicesItem from '../app/components/ServicesItem.vue'
+import AboutUs from '../app/components/AboutUs.vue'
+import TaxiFleet from '../app/components/TaxiFleet.vue'
+import HomeUs from '../app/components/HomeUs.vue'
 
 const routes = [{
-    path: '/services',
-    name: 'Services',
-    component: Services
+    path: '/servicesitem',
+    name: 'ServicesItem',
+    component: ServicesItem
+   },
+   {
+    path: '/taxifleet',
+    name: 'TaxiFleet',
+    component: TaxiFleet
+   },
+   {
+    path: '/aboutus',
+    name: 'AboutUs',
+    component: AboutUs
+   },
+   {
+    path: '/',
+    name: 'HomeUs',
+    component: HomeUs
    }
+
 ]
 
 const router = createRouter({
