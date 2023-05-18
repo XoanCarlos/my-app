80691c422e065fc4ad5abcdd1cc3749ebe1ee3f5
jcarlos <jcarlosperezgonzalez@gmail.com>
2023-05-18 20:45:38 +0200

    sadf

--- src/router/index.js
+++ src/router/index.js
@@ -11,33 +11,38 @@ const routes = [{
     name: 'ServicesItem',
     component: ServicesItem
    },
+   
    {
     path: '/taxifleet',
     name: 'TaxiFleet',
     component: TaxiFleet
    },
+   
    {
     path: '/aboutus',
     name: 'AboutUs',
     component: AboutUs
    },
+
    {
     path: '/',
     name: 'HomeUs',
     component: HomeUs
    },
+
    {
      path: "/:catchAll(.*)",
      name: "NotFound",
      component: NotFound
 >>>>>>> f1e47cddf9645a34b70e1bd49c84315721e39984
    }
-
+   
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
 
 export default router;
